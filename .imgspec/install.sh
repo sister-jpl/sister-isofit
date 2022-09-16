47916fdf68cb062e4be049d1bab79a96fc05945c
Winston Olson-Duvall <winston.olson-duvall@jpl.nasa.gov>
2022-09-16 09:45:32 -0700

    Check in sister-dev updates again.

--- .imgspec/install.sh
+++ .imgspec/install.sh
@@ -1,10 +1,8 @@
-run_dir=$('pwd')
 imgspec_dir=$(cd "$(dirname "$0")" ; pwd -P)
 pge_dir=$(dirname ${imgspec_dir})
+app_dir=$(dirname ${pge_dir})
 
-# Need to do custom install to prevent dependency errors
-conda create -y --name sister python=3.7
-source activate sister
-git clone https://github.com/isofit/isofit.git
+cd $app_dir
+git clone https://github.com/isofit/isofit.git -b v2.9.1
 cd isofit
-pip install .
+pip install -e .
