aa4f2eb8d36bed81fa2071c9e1233fe9ca77873b
Adam Chlus <23018310+adamchlus@users.noreply.github.com>
2023-03-30 10:10:55 -0700

    Revert back to AV instruments

--- sister_isofit.py
+++ sister_isofit.py
@@ -126,10 +126,10 @@ def main():
         sensor = 'emit'
         temp_basename = f'{sensor}{rdn_basename.split("_")[4]}'
         surface_config = "work/emit_surface_20221001.json"
-    elif instrument == "XAVNG":
+    elif instrument == "AVNG":
         sensor = 'ang'
         temp_basename = f'{sensor}{rdn_basename.split("_")[4]}'
-    elif instrument == "XAVCL":
+    elif instrument == "AVCL":
         sensor = 'avcl'
         temp_basename = f'f{rdn_basename.split("_")[4][2:8]}t00p00r00'
     else:
